aa29c9ffccab76ba4bfa192a83a8a5ff8e679bc0
GravityTwoG <crytekov@gmail.com>
2024-08-09 20:12:38 +0700

    feat: now CreateChatButton is located in Chat itself, so we don't need this button here

--- packages/ui/src/components/sidebar/buttons/styled.tsx
+++ packages/ui/src/components/sidebar/buttons/styled.tsx
@@ -1,6 +1,5 @@
 import { styled } from 'styled-components';
 import { Button } from '@/ui/components/button';
-import { Plus1Icon } from '@/ui/icons/plus-1';
 import { TrashIcon } from '@/ui/icons/trash';
 
 export interface SidebarButtonsStyledProps {
@@ -14,13 +13,6 @@ export const SidebarButtonsStyled = styled.div<SidebarButtonsStyledProps>`
   transition: all 0.3s;
   width: 100%;
   gap: 12px;
-`;
-
-export const SidebarCreateChatButton = styled(Button).attrs({
-  startIcon: <Plus1Icon />,
-  fullWidth: true
-})`
-  white-space: nowrap;
 `;
 
 export const SidebarDeleteChatsButton = styled(Button).attrs({
