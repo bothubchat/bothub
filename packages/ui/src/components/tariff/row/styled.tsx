a34db3df976b67495d7abec7ecd20d5699b0dfe1
wqr1 <dimakond520@gmail.com>
2024-08-19 19:08:32 +0400

    Small code refactor to eliminate eslint warnings

--- packages/ui/src/components/tariff/row/styled.tsx
+++ packages/ui/src/components/tariff/row/styled.tsx
@@ -1,9 +1,10 @@
-import styled, { css } from "styled-components";
+import styled, { css } from 'styled-components';
 import { Typography } from '@/ui/components/typography';
 import { Badge } from '@/ui/components/badge';
-import { Radio } from "@/ui/components/radio";
-import { CheckCircleIcon } from "@/ui/icons/check-circle";
+import { Radio } from '@/ui/components/radio';
+import { CheckCircleIcon } from '@/ui/icons/check-circle';
 import { TariffCardVariant, TariffCardColor } from './types';
+
 export interface TariffCardProps {
   $color?: TariffCardColor;
   selected?: boolean;
@@ -24,42 +25,49 @@ export const TariffCardStyled = styled.div<TariffCardProps>`
   & > input:[type="radio"]:checked ~ & {
     outline: 1px solid ${({ theme }) => theme.default.colors.accent.primary};
   }
-  ${({ selected, theme }) => (
-    selected && theme.mode === 'light' && css`
-    outline: 1px solid ${theme.default.colors.accent.primary};
-    background-color: ${theme.colors.grayScale.gray3};
-    `
-  )}
-  ${({ selected, theme }) => (
-    selected && theme.mode === 'dark' && css`
-    outline: 1px solid ${theme.default.colors.accent.primary};
-    background-color: ${theme.colors.grayScale.gray3};
-    `
-  )}
-  ${({theme}) => (
-    theme.mode === 'dark' && css`
+  ${({ selected, theme }) =>
+    selected &&
+    theme.mode === 'light' &&
+    css`
+      outline: 1px solid ${theme.default.colors.accent.primary};
+      background-color: ${theme.colors.grayScale.gray3};
+    `}
+  ${({ selected, theme }) =>
+    selected &&
+    theme.mode === 'dark' &&
+    css`
+      outline: 1px solid ${theme.default.colors.accent.primary};
+      background-color: ${theme.colors.grayScale.gray3};
+    `}
+  ${({ theme }) =>
+    theme.mode === 'dark' &&
+    css`
       &:after {
-      display: block;
-      content: '';
-      position: absolute;
-      width: 1070px;
-      height: 216.23px;
-      gap: 0px;
-      left: -240px;
-      top: -200px;
-      filter: blur(50px);
-      transform: rotate(-4.41deg);
-      box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
-      background: linear-gradient(90.98deg, #2244F4 7.53%, rgba(252, 94, 255, 0.4) 72.48%);
-      border-radius: 50%;
-      z-index: -1;
-    }`
-  )};
+        display: block;
+        content: '';
+        position: absolute;
+        width: 1070px;
+        height: 216.23px;
+        gap: 0px;
+        left: -240px;
+        top: -200px;
+        filter: blur(50px);
+        transform: rotate(-4.41deg);
+        box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
+        background: linear-gradient(
+          90.98deg,
+          #2244f4 7.53%,
+          rgba(252, 94, 255, 0.4) 72.48%
+        );
+        border-radius: 50%;
+        z-index: -1;
+      }
+    `};
 
   @media (max-width: 550px) {
     padding: 14px;
   }
-`
+`;
 
 export const TariffCardStyledContent = styled.div`
   position: relative;
@@ -68,14 +76,14 @@ export const TariffCardStyledContent = styled.div`
   justify-content: space-between;
   align-items: center;
   gap: 34px;
-    @media (max-width: 1060px) {
+  @media (max-width: 1060px) {
     gap: 32px;
   }
   @media (max-width: 550px) {
     flex-wrap: wrap;
     gap: 10px;
   }
-`
+`;
 
 export const TarrifCardStyledLeft = styled.div`
   position: relative;
@@ -86,9 +94,15 @@ export const TarrifCardStyledLeft = styled.div`
   @media (max-width: 1060px) {
     min-width: 200px;
   }
-`
+`;
 
-export const TariffCardStyledName: React.FC<{$color: TariffCardColor, children?: React.ReactNode}> = styled(Typography).attrs({ variant: 'body-xl-semibold', component: 'h3' })<{$color: TariffCardColor}>`
+export const TariffCardStyledName: React.FC<{
+  $color: TariffCardColor;
+  children?: React.ReactNode;
+}> = styled(Typography).attrs({
+  variant: 'body-xl-semibold',
+  component: 'h3',
+})<{ $color: TariffCardColor }>`
   background-clip: text;
   -webkit-background-clip: text;
   width: fit-content;
@@ -123,13 +137,15 @@ export const TariffCardStyledMiddle = styled.div`
   gap: 10px;
 `;
 
-export const TariffCardStyledDescription = styled(Typography).attrs({ variant: 'body-s-medium' })`
+export const TariffCardStyledDescription = styled(Typography).attrs({
+  variant: 'body-s-medium',
+})`
   width: 100%;
   max-width: 518px;
   overflow: hidden;
   display: -webkit-box;
   -webkit-line-clamp: 3;
-  line-clamp: 3; 
+  line-clamp: 3;
   -webkit-box-orient: vertical;
 `;
 
@@ -142,9 +158,13 @@ export const TarrifCardStyledRight = styled.div`
   }
 `;
 
-export const TariffCardStyledPrice = styled(Typography).attrs({ variant: 'body-xxl-semibold' })``;
+export const TariffCardStyledPrice = styled(Typography).attrs({
+  variant: 'body-xxl-semibold',
+})``;
 
-export const TariffCardStyledCurrency = styled(Typography).attrs({ variant: 'body-l-medium' })``;
+export const TariffCardStyledCurrency = styled(Typography).attrs({
+  variant: 'body-l-medium',
+})``;
 
 export const TarrifCardStyledRadio = styled(Radio)``;
 
@@ -161,18 +181,24 @@ export const TariffCardGiveCapsBadge = styled(Badge).attrs({ variant: 'info' })`
   `}
 `;
 
-export const TariffCardGiveCapsBadgeText = styled(Typography).attrs({ variant: 'body-s-regular' })`
+export const TariffCardGiveCapsBadgeText = styled(Typography).attrs({
+  variant: 'body-s-regular',
+})`
   white-space: nowrap;
   ${({ theme }) => `
     color: ${theme.default.colors.base.white};
   `}
 `;
 
-export const TariffCardStyledDescriptionIcon = styled(CheckCircleIcon).attrs({ size: 18 })`
+export const TariffCardStyledDescriptionIcon = styled(CheckCircleIcon).attrs({
+  size: 18,
+})`
   @media (max-width: 550px) {
     display: none;
   }
 `;
-export const TariffCardGiveCapsText = styled(Typography).attrs({ variant: 'body-xs-regular' })`
+export const TariffCardGiveCapsText = styled(Typography).attrs({
+  variant: 'body-xs-regular',
+})`
   white-space: nowrap;
-`
+`;
