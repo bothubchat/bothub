f1fe4d43ed43ef7842d8b3e77f279369d081b6d3
GravityTwoG <crytekov@gmail.com>
2024-08-19 21:45:22 +0700

    feat: add formatted value

--- packages/ui/src/components/range-field/index.stories.tsx
+++ packages/ui/src/components/range-field/index.stories.tsx
@@ -12,15 +12,36 @@ export const Basic: RangeFieldStory = {
   args: {
     step: 0.1,
     min: 0,
-    max: 100
-  }
+    max: 100,
+  },
 };
 
 export const Label: RangeFieldStory = {
   args: {
     ...Basic.args,
-    label: 'Label'
-  }
+    label: 'Label',
+  },
+};
+
+export const FormattedValue: RangeFieldStory = {
+  args: {
+    ...Basic.args,
+    label: 'Количество символов',
+    min: 600,
+    max: 7200,
+    step: 200,
+    fullWidth: true,
+    formatValue: (value) => {
+      const v = typeof value === 'number' ? value : value[0];
+      return (
+        `${v} - ${v * 2} символов / ${Math.round(
+          v / 6
+        )} - ${Math.round(v / 3)} слов (${Math.max(Math.round(
+          v / 3600
+        ), 1)} - ${Math.max(Math.round(v / 1800), 1)} страниц)`
+      );
+    },
+  },
 };
 
 export const Help: RangeFieldStory = {
@@ -29,42 +50,38 @@ export const Help: RangeFieldStory = {
     label: (
       <RangeFieldLabel>
         Label
-        <Tooltip
-          label="Help"
-        >
-          <Button
-            variant="help"
-          />
+        <Tooltip label="Help">
+          <Button variant="help" />
         </Tooltip>
       </RangeFieldLabel>
-    )
-  }
+    ),
+  },
 };
 
 export const Error: RangeFieldStory = {
   args: {
     ...Label.args,
-    error: 'Error message'
-  }
+    error: 'Error message',
+  },
 };
 
 export const Disabled: RangeFieldStory = {
   args: {
     ...Label.args,
     value: 50,
-    disabled: true
-  }
+    disabled: true,
+  },
 };
 
 export const Skeleton: RangeFieldStory = {
   args: {
     ...Label.args,
-    skeleton: true
-  }
+    skeleton: true,
+  },
 };
 
 export default {
   title: 'UI Components/Fields/Range',
   component: RangeField,
-  decorators: [StoryDecorator({ margin: '50px 0px' })]
+  decorators: [StoryDecorator({ margin: '50px 0px' })],
 } as RangeFieldMeta;
