b0127a1aef73bee7f169485118e92cca6c0dced7
GravityTwoG <crytekov@gmail.com>
2024-09-13 17:30:25 +0700

    feat: add Google, Yandex, VK icons

--- packages/ui/src/stories/icons/index.tsx
+++ packages/ui/src/stories/icons/index.tsx
@@ -169,7 +169,7 @@ import {
   YandexIcon,
   VKIcon,
   ArticleGeneratorIcon,
-  SuccessIcon
+  SuccessIcon,
 } from '@/ui/icons';
 import { IconItem } from './item';
 import { IconProvider, IconProviderProps } from '@/ui/components/icon';
