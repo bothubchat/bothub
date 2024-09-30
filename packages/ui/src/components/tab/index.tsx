e4716f0074a413ec18a398c67f79e49c69d86e5c
GravityTwoG <crytekov@gmail.com>
2024-09-30 21:36:08 +0700

    feat: move animations to css

--- packages/ui/src/components/tab/index.tsx
+++ packages/ui/src/components/tab/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { TabStyled, TabText } from './styled';
-import { useTheme } from '@/ui/theme';
 
 export interface TabProps extends React.PropsWithChildren {
   className?: string;
@@ -10,47 +9,22 @@ export interface TabProps extends React.PropsWithChildren {
 
 export const Tab: React.FC<TabProps> = ({
   className, active = false, children, onClick
-}) => {
-  const theme = useTheme();
-
-  return (
-    <TabStyled
-      $active={active}
-      variants={{
-        default: {
-          borderColor: theme.colors.grayScale.gray3,
-          backgroundColor: 'rgba(0, 0, 0, 0)'
-        },
-        hover: {
-          borderColor: theme.colors.grayScale.gray3,
-          backgroundColor: theme.colors.grayScale.gray3
-        },
-        active: {
-          borderColor: theme.colors.accent.primary,
-          backgroundColor: theme.colors.accent.primary
-        },
-        tap: {
-          y: 1
-        }
-      }}
-      initial={active ? 'active' : 'default'}
-      animate={active ? 'active' : 'default'}
-      whileHover={!active ? 'hover' : {}}
-      whileTap={!active ? 'tap' : {}}
-      className={className}
-      onClick={onClick}
-    >
-      {typeof children !== 'string' && children}
-      {typeof children === 'string' && (
-        <TabText
-          $active={active}
-        >
-          {children}
-        </TabText>
-      )}
-    </TabStyled>
-  );
-};
+}) => (
+  <TabStyled
+    $active={active}
+    className={className}
+    onClick={onClick}
+  >
+    {typeof children !== 'string' && children}
+    {typeof children === 'string' && (
+      <TabText
+        $active={active}
+      >
+        {children}
+      </TabText>
+    )}
+  </TabStyled>
+);
 
 export * from './styled';
 export * from './list';
