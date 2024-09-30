7667e935c0795f4825a4005e6b447f86854a3c17
GravityTwoG <crytekov@gmail.com>
2024-09-30 21:58:34 +0700

    feat: move animations to css

--- packages/ui/src/components/bookmark/index.tsx
+++ packages/ui/src/components/bookmark/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useTheme } from '@/ui/theme';
 import { BookmarkCloseButton, BookmarkName, BookmarkStyled } from './styled';
 import { Skeleton } from '@/ui/components/skeleton';
 
@@ -22,33 +21,13 @@ export type BookmarkProps = (BookmarkDefaultProps | BookmarkSkeletonProps) & {
 export const Bookmark: React.FC<BookmarkProps> = ({
   className, ...props
 }) => {
-  const theme = useTheme();
   const isActive = (props.skeleton ? false : props.active) ?? false;
 
   return (
     <BookmarkStyled 
       $active={isActive}
+      $skeleton={props.skeleton}
       className={className}
-      variants={{
-        default: {},
-        skeleton: {
-          boxShadow: [
-            `0px 0px 0px 1px ${theme.colors.grayScale.gray1}`,
-            `0px 0px 0px 1px ${theme.colors.grayScale.gray5}`
-          ],
-          transition: {
-            duration: 1,
-            repeat: Infinity,
-            repeatType: 'reverse'
-          }
-        },
-        hover: {
-          boxShadow: `0px 0px 0px 1px ${theme.colors.grayScale.gray3}`
-        }
-      }}
-      initial={props.skeleton ? 'skeleton' : 'default'}
-      animate={props.skeleton ? 'skeleton' : 'default'}
-      whileHover={!props.skeleton && !isActive ? 'hover' : 'default'}
       onClick={props.onClick}
     >
       <BookmarkName>
