0a6eb066170845653b18b230cdf47289bf2c6b5e
GravityTwoG <crytekov@gmail.com>
2024-10-01 20:46:58 +0700

    feat: use react-spring for presence animation

--- packages/ui/src/components/scrollbar/shadow/index.tsx
+++ packages/ui/src/components/scrollbar/shadow/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useTransition } from '@react-spring/web';
 import { ScrollbarShadowContent, ScrollbarShadowStyled } from './styled';
 import { ScrollbarShadowSide } from './types';
 import { useScrollbar } from '../context';
@@ -10,23 +11,31 @@ export interface ScrollbarShadowProps {
   color?: string;
 }
 
-export const ScrollbarShadow: React.FC<ScrollbarShadowProps> = ({ side, ...props }) => {
+export const ScrollbarShadow: React.FC<ScrollbarShadowProps> = ({
+  side,
+  ...props
+}) => {
   const theme = useTheme();
-  const { scrollbarSize, scrollShadows, disabled } = useScrollbar();
+  const {
+    scrollbarSize,
+    scrollShadows,
+    disabled,
+    isLeft: isLeftVisible,
+    isRight: isRightVisible,
+    isTop: isTopVisible,
+    isBottom: isBottomVisible,
+  } = useScrollbar();
 
   const size: number = props.size ?? scrollShadows?.size ?? 60;
   const color: string = props.color ?? scrollShadows?.color ?? theme.colors.grayScale.gray4;
+  
   const isLeft: boolean = side === 'left';
   const isRight: boolean = side === 'right';
   const isTop: boolean = side === 'top';
   const isBottom: boolean = side === 'bottom';
 
-  const style: Record<string, string> = {
-    background: `linear-gradient(to ${side}, rgba(0, 0, 0, 0), ${color})`
-  };
-
   const initialStyle: Record<string, string | number> = {
-    opacity: 0
+    opacity: 0,
   };
   if (isLeft || isRight) {
     initialStyle.width = 0;
@@ -35,7 +44,7 @@ export const ScrollbarShadow: React.FC<ScrollbarShadowProps> = ({ side, ...props
   }
 
   const animateStyle: Record<string, string | number> = {
-    opacity: 1
+    opacity: 1,
   };
   if (isLeft || isRight) {
     initialStyle.width = size;
@@ -43,19 +52,32 @@ export const ScrollbarShadow: React.FC<ScrollbarShadowProps> = ({ side, ...props
     initialStyle.height = size;
   }
 
-  return (
-    <ScrollbarShadowStyled 
-      $side={side}
-      $scrollbarSize={scrollbarSize}
-      $disabled={disabled}
-    >
-      <ScrollbarShadowContent
-        style={style}
-        initial={initialStyle}
-        animate={animateStyle}
-        exit={initialStyle}
-      />
-    </ScrollbarShadowStyled>
+  const visible = (isLeftVisible && isLeft)
+    || (isRightVisible && isRight)
+    || (isTopVisible && isTop)
+    || (isBottomVisible && isBottom);
+
+  const transition = useTransition(visible, {
+    from: initialStyle,
+    enter: animateStyle,
+    leave: initialStyle,
+  });
+
+  return transition(
+    (style, item) => item && (
+      <ScrollbarShadowStyled
+        $side={side}
+        $scrollbarSize={scrollbarSize}
+        $disabled={disabled}
+      >
+        <ScrollbarShadowContent
+          style={{
+            ...style,
+            background: `linear-gradient(to ${side}, rgba(0, 0, 0, 0), ${color})`,
+          }}
+        />
+      </ScrollbarShadowStyled>
+    )
   );
 };
 
