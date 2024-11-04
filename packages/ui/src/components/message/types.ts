3c1ba4b5bc38e2603781f14417e47695ccbab318
WQR1 <dimakond520@gmail.com>
2024-11-05 00:41:21 +0400

    fixed type inconvenience

--- packages/ui/src/components/message/types.ts
+++ packages/ui/src/components/message/types.ts
@@ -3,5 +3,6 @@ export type MessageVariant = 'user' | 'assistant';
 export type MessageColor = string | 'default' | 'green' | 'purple';
 
 export type MessageCopyEventHandler = (data: ClipboardItem[]) => unknown;
+export type MessageContextCopyEventHandler = () => unknown;
 
 export type MessageCodeCopyEventHandler = (code: string) => unknown;
