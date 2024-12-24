784c929d539707c260919408168d96ac5df5a91d
GravityTwoG <crytekov@gmail.com>
2024-12-24 15:32:46 +0700

    feat: load subset of languages

---
+++ packages/ui/src/components/message/components/code/multiline/content/highlight.tsx
@@ -0,0 +1,64 @@
+import React, {
+  useEffect,
+  useRef,
+  ReactNode,
+  ElementType,
+  useCallback,
+} from 'react';
+import { hljs } from './languages';
+
+export interface HighlightProps {
+  children: ReactNode;
+  className?: string;
+  element?: ElementType;
+  innerHTML?: boolean;
+}
+
+export const Highlight: React.FC<HighlightProps> = ({
+  children,
+  className = '',
+  element: Element,
+  innerHTML = false,
+}) => {
+  const elRef = useRef<HTMLDivElement | HTMLPreElement>(null);
+
+  const highlightCode = useCallback(() => {
+    if (!elRef.current) return;
+    const nodes = elRef.current.querySelectorAll('pre code');
+
+    for (let i = 0; i < nodes.length; i++) {
+      hljs.highlightElement(nodes[i] as HTMLElement);
+    }
+  }, []);
+
+  useEffect(() => {
+    highlightCode();
+  }, [className, children, highlightCode]);
+
+  if (innerHTML) {
+    const props = {
+      ref: elRef,
+      className,
+      dangerouslySetInnerHTML: { __html: children as string },
+    };
+    if (Element) {
+      return <Element {...props} />;
+    }
+
+    return <div {...props} ref={elRef as React.RefObject<HTMLDivElement>} />;
+  }
+
+  if (Element) {
+    return (
+      <Element ref={elRef} className={className}>
+        {children}
+      </Element>
+    );
+  }
+
+  return (
+    <pre ref={elRef as React.RefObject<HTMLPreElement>}>
+      <code className={className}>{children}</code>
+    </pre>
+  );
+};
--- packages/ui/src/components/message/components/code/multiline/content/highlighted.ts
+++ packages/ui/src/components/message/components/code/multiline/content/highlighted.ts
@@ -1,11 +1,8 @@
 import styled from 'styled-components';
-import HL from 'react-highlight';
-import { interopDefaultCJSImport } from '@/ui/utils';
 import { contentStyles } from './styled';
 import { MessageMultilineCodeContentProps } from './types';
+import { Highlight } from './highlight';
 import 'highlight.js/styles/base16/atlas.min.css';
-
-const Highlight = interopDefaultCJSImport<typeof HL>(HL);
 
 export const MessageMultilineCodeContentHighlighted = styled(
   Highlight
