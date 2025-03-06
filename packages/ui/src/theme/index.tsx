547d991fb7ebf55a05f8de3c2b9b0613cd90daa1
Timur Basyrov <timyr.278@gmail.com>
2025-03-06 20:08:30 +0500

    feat: add new colors in theme

--- packages/ui/src/theme/index.tsx
+++ packages/ui/src/theme/index.tsx
@@ -40,6 +40,15 @@ export const defaultTheme: DefaultTheme = {
       gray7: '#171E2F'
     },
     premiumGradient: 'linear-gradient(90deg, #1C64F2 -0.39%, #D41CF2 99.61%)',
+    gradient: {
+      basic: 'linear-gradient(90deg, #00247D 0%, #1C64F2 100%)',
+      light: 'linear-gradient(90deg, #073DC3 0%, #4785FF 100%)',
+      premium: 'linear-gradient(90deg, #4785FF 0%, #7740F2 100%)',
+      deluxe: 'linear-gradient(90deg, #5728FF 0%, #A750FF 100%)',
+      elite: 'linear-gradient(90deg, #1C64F2 100%, #D41CF2 100%)',
+      elite20:
+        'linear-gradient(90deg, rgba(28,100,242,0.2) 0%, rgba(212,28,242,0.2) 100%)'
+    },
     critic: '#FE4242',
     orange: '#F29C1C',
     purple: '#941CF2',
