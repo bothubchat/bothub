f5086ae7803b58f7e8455d07a364ee24842770a4
Yan Turnt <bx6xbchosen1@gmail.com>
2025-05-27 15:14:09 +0600

    feat: added div props for tag badge

--- packages/ui/src/components/tag-badge/index.tsx
+++ packages/ui/src/components/tag-badge/index.tsx
@@ -1,9 +1,9 @@
+import { ComponentProps } from 'react';
 import * as S from './styled';
 
 export type TagBadgeProps = {
-  children: string;
   color?: string;
-};
+} & ComponentProps<'div'>;
 
 export const TagBadge = ({ children, color }: TagBadgeProps) => (
   <S.TagBadgeStyled $color={color}>
