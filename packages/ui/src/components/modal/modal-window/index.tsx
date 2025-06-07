c4cbe4b853ad276842f78c57224b56edd556c973
Yan Turnt <bx6xbchosen1@gmail.com>
2025-06-07 10:56:06 +0600

    feat: added ref for modal window

--- packages/ui/src/components/modal/modal-window/index.tsx
+++ packages/ui/src/components/modal/modal-window/index.tsx
@@ -1,5 +1,5 @@
 import { useTransition } from '@react-spring/web';
-import type { PropsWithChildren, ReactNode } from 'react';
+import { forwardRef, type PropsWithChildren, type ReactNode } from 'react';
 import * as S from './styled';
 
 export type ModalCloseEventHandler = () => unknown;
@@ -13,54 +13,60 @@ export type ModalWindowlProps = {
   onClose?: ModalCloseEventHandler;
 } & PropsWithChildren;
 
-export const ModalWindow = ({
-  children,
-  open,
-  title = null,
-  scrollbar = false,
-  images,
-  className,
-  onClose
-}: ModalWindowlProps) => {
-  const modalTransition = useTransition(open, {
-    from: { opacity: 0, transform: 'scale(0.9)' },
-    enter: { opacity: 1, transform: 'scale(1)' },
-    leave: { opacity: 0, transform: 'scale(0.9)' },
-    config: { duration: 200 }
-  });
+export const ModalWindow = forwardRef<HTMLDivElement, ModalWindowlProps>(
+  (
+    {
+      children,
+      open,
+      title = null,
+      scrollbar = false,
+      images,
+      className,
+      onClose
+    },
+    ref
+  ) => {
+    const modalTransition = useTransition(open, {
+      from: { opacity: 0, transform: 'scale(0.9)' },
+      enter: { opacity: 1, transform: 'scale(1)' },
+      leave: { opacity: 0, transform: 'scale(0.9)' },
+      config: { duration: 200 }
+    });
 
-  const content = <S.ModalWindowContent>{children}</S.ModalWindowContent>;
+    const content = <S.ModalWindowContent>{children}</S.ModalWindowContent>;
 
-  return modalTransition(
-    (style, item) =>
-      item && (
-        <S.ModalWindowStyled
-          style={style}
-          className={className}
-        >
-          {images}
-          <S.ModalWindowBody>
-            {typeof title === 'string' ? (
-              <S.ModalWindowTitle>{title}</S.ModalWindowTitle>
-            ) : (
-              title
-            )}
-            <S.ModalWindowCloseButton onClick={onClose}>
-              <S.ModalWindowCloseButtonIcon size={24} />
-            </S.ModalWindowCloseButton>
-            <S.ModalWindowBodyContent>
-              {scrollbar ? (
-                <S.ModalWindowBodyScrollbarWrapper
-                  overflow={scrollbar ? 'auto' : 'visible'}
-                >
-                  {content}
-                </S.ModalWindowBodyScrollbarWrapper>
+    return modalTransition(
+      (style, item) =>
+        item && (
+          <S.ModalWindowStyled
+            ref={ref}
+            style={style}
+            className={className}
+          >
+            {images}
+            <S.ModalWindowBody>
+              {typeof title === 'string' ? (
+                <S.ModalWindowTitle>{title}</S.ModalWindowTitle>
               ) : (
-                content
+                title
               )}
-            </S.ModalWindowBodyContent>
-          </S.ModalWindowBody>
-        </S.ModalWindowStyled>
-      )
-  );
-};
+              <S.ModalWindowCloseButton onClick={onClose}>
+                <S.ModalWindowCloseButtonIcon size={24} />
+              </S.ModalWindowCloseButton>
+              <S.ModalWindowBodyContent>
+                {scrollbar ? (
+                  <S.ModalWindowBodyScrollbarWrapper
+                    overflow={scrollbar ? 'auto' : 'visible'}
+                  >
+                    {content}
+                  </S.ModalWindowBodyScrollbarWrapper>
+                ) : (
+                  content
+                )}
+              </S.ModalWindowBodyContent>
+            </S.ModalWindowBody>
+          </S.ModalWindowStyled>
+        )
+    );
+  }
+);
