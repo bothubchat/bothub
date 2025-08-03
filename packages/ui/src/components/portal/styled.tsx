0c85e2789c2db1a3c3641892f551312c2d30b768
UnicornNaughty <gccccvbnnnn@mail.ru>
2025-08-03 16:48:10 +0700

    fix: portal bug

--- packages/ui/src/components/portal/styled.tsx
+++ packages/ui/src/components/portal/styled.tsx
@@ -2,4 +2,6 @@ import { styled } from 'styled-components';
 
 export const portalId = 'bothub_portal';
 
-export const PortalElement = styled.div.attrs({ id: portalId })``;
+export const PortalElement = styled.div.attrs({ id: portalId })`
+  position: absolute;
+`;
